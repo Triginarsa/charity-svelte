1f771de57dd0a6246ddb34fb5525054dec6f4073
adi <adi@cakeplabs.com>
2021-10-23 18:16:35 +0800

    Change HTML to Svelte Component

---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports = {
+    charities: [
+        {
+            id: 1,
+            title: 'First Charity Project',
+            category : 'Money',
+            thumbnail: 'https://live.staticflickr.com/3441/5833158524_7413f44a3d_o_d.jpg', 
+            pledged: 99000,
+            target: 100000,
+            date_end: +new Date('30 October 2021'),
+            profile_photo: 'https://live.staticflickr.com/4027/4357722810_7136f4a9e9_s.jpg',
+            profile_name: 'Adi',
+            no_pledges: 0
+        }
+    ]
+}
